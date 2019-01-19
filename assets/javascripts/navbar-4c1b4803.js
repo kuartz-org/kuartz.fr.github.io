9b9beb0628d218051a938594cf0537602da2978a
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2019-01-19 11:41:20 +0100

    Deployed using middleman-deploy v2.0.0-alpha

---
+++ assets/javascripts/navbar-4c1b4803.js
@@ -0,0 +1,52 @@
+import smoothscroll from 'smoothscroll-polyfill'
+
+// kick off the polyfill!
+smoothscroll.polyfill()
+
+const navbar       = document.querySelector('.navbar')
+const navbarHeight = navbar.offsetHeight
+const navbarItems  = navbar.querySelectorAll('.navbar-menu .navbar-item')
+
+navbarItems.forEach(item => {
+  item.addEventListener('click', event => {
+    event.preventDefault()
+    const section = document.querySelector(item.getAttribute('href'))
+    
+    setActiveNavbarItem(item)
+    window.scroll({ top: section.offsetTop - navbar.offsetHeight, left: 0, behavior: 'smooth' })
+  })
+})
+
+document.addEventListener('scroll', event => {
+  setActiveNavbarItem(getActiveNavbarItem())
+})
+
+setActiveNavbarItem(getActiveNavbarItem())
+
+function getActiveNavbarItem() {
+  const windowPosition = window.pageYOffset
+  const windowHeight   = window.innerHeight
+  const bodyHeight     = document.querySelector('body').offsetHeight
+
+  let activeNavbarItem
+  Array.from(navbarItems).forEach(item => {
+    const section      = document.querySelector(item.getAttribute('href'))
+    const itemPosition = section.offsetTop - navbarHeight
+    if (itemPosition <= windowPosition) {
+      activeNavbarItem = item
+    }
+  })
+
+  if (windowPosition === 0) {
+    activeNavbarItem = navbarItems[0]
+  } else if (windowPosition === bodyHeight - windowHeight) {
+    activeNavbarItem = navbarItems[navbarItems.length - 1]
+  }
+
+  return activeNavbarItem
+}
+
+function setActiveNavbarItem(activeNavbarItem) {
+  navbarItems.forEach(item => item.classList.remove('active'))
+  activeNavbarItem.classList.add('active')
+}
