77adf94039a5d587b675f41b28fa30cb845cbaa0
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2019-07-05 16:20:13 +0200

    Deployed using middleman-deploy v2.0.0-alpha

--- assets/javascripts/application-8cf269f4.js
+++ assets/javascripts/application-8cf269f4.js
@@ -1,3 +1,4 @@
 import '../../../node_modules/bootstrap/dist/js/bootstrap.min';
 import './navbar';
 import './portfolio';
+import './modules/swipe';
---
+++ assets/javascripts/modules/swipe-554db2a0.js
@@ -0,0 +1,29 @@
+import Swipe from '../../../../node_modules/swipejs/swipe';
+
+const slider = document.getElementById('slider');
+
+if (slider) {
+  const itemsCount = document.querySelectorAll('.swipe-wrap > div').length;
+  const randomPosition = Math.floor(Math.random() * itemsCount);
+
+  window.mySwipe = new Swipe(slider, {
+    startSlide:      randomPosition, // index position at which Swipe should start.
+    speed:           1000,           // speed of prev and next transitions in milliseconds.
+    auto:            7000,           // when specified, start an auto-playing slideshow (time in milliseconds between slide change).
+    continuous:      true,           // create an infinite feel with no endpoints.
+    autoRestart:     false,          // auto restart slideshow after user's touch event or next/prev calls.
+    disableScroll:   false,          // prevent any touch events on this container from scrolling the page.
+    stopPropagation: false,          // stop event propagation.
+    draggable:       true,           // listen to mouse events in addition to the touch events.
+  
+    callback: function(index, elem, dir) {
+      // runs at slide change. Three parameters are passed to the function: index (the current slide index)elem (the current slide element) and dir (direction: 1 for left or backward-1 for right or forward).
+      
+    },
+  
+    transitionEnd: function(index, elem) {
+      // runs at the end of a slide transition. Two parameters are passed to the function: index (the current slide index) and elem (the current slide element).
+  
+    }
+  });
+}
